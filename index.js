f1cbbee5cbac0efa97652f8c93b1932595ba7247
Cheng Vue <csalvue@gmail.com>
2019-02-23 14:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Write your solution in this file!
-var customerName = "bob"
+var customerName = "bob";
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
 function setBestCustomer() {
-  bestCustomer = "not bob"
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+
 }
